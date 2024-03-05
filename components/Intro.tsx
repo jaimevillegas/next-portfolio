dabd9289d41a34cd56997d85bdc52a34155ff836
Jaime Villegas <jaimevillegas296@gmail.com>
2024-03-04 21:14:30 -0500

    Add Intro component and configure next.config.js for remote images

---
+++ components/Intro.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+
+function Intro() {
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            tansition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+              alt="Jaime"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.2,
+            }}
+            className="text-4xl absolute bottom-0 right-0"
+          >
+            🚀
+          </motion.span>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Intro;
