d2f670ad1b0011ef19ed16d9dd0d2dd94ad808f5
Jaime Villegas <jaimevillegas296@gmail.com>
2024-03-09 16:58:58 -0500

    Refactor layout and add scroll animation to Projects component

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function SectionHeader({ children }: { children: string }) {
-  return <h2 className="text-3xl mb-8 font-medium capitalize">{children}</h2>;
+  return (
+    <h2 className="text-3xl text-center mb-8 font-medium capitalize">
+      {children}
+    </h2>
+  );
 }
 
 export default SectionHeader;
